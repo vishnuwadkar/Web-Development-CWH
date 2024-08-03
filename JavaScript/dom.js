bbf750563d256b9d6a224b12e0ec3b9dc1d21d41
vishnuwadkar <vishnuwadkar2004@gmail.com>
2024-08-04 00:57:50 +0530

    DOM added

---
+++ JavaScript/dom.js
@@ -0,0 +1,8 @@
+// console.dir() -> prints properties and methods of special objects
+// window is a global object 
+
+//window and window.document are same since window is an global object
+
+//document.body.childNodes[n] ->accessing child nodes
+
+document.body.childNodes[1].innerHTML = "Hello World!"      //altering the inner html of an element!
