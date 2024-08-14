8dd779ac2fcc6f3a4c68b5561f6ac4c3783de148
vishnuwadkar <vishnuwadkar2004@gmail.com>
2024-08-15 01:33:17 +0530

    APIs done!

---
+++ JavaScript/api.js
@@ -0,0 +1,33 @@
+const URL ="https://cat-fact.herokuapp.com/facts"
+const fact = document.querySelector(".fact")
+const fact2 = document.querySelector(".fact2")
+const btn = document.querySelector("#btn")
+const btn2 = document.querySelector("#btn2")
+let data
+
+const getFacts = async ()=>{
+    console.log("getting data...")
+    let response = await fetch(URL)   //fetch method(get)
+    console.log(response)
+    data = await response.json();   //convert into json format
+    // console.log(data[0].text)   //text is method in the returned object
+    for(let i=0; i<5;i++){
+        fact.innerHTML += data[i].text+"<br>"
+    }
+}
+btn.addEventListener("click",getFacts)
+
+//doing the same with promise chaining 
+function getFacts2(){
+    return fetch(URL)
+    .then((response)=>{
+        return response.json 
+    })
+    .then((data)=>{
+        console.log(data)
+        for(let i=0; i<5;i++){
+            fact2.innerHTML += data[i].text+"<br>"
+        }
+    })
+}
+    btn2.addEventListener("click",getFacts2)
